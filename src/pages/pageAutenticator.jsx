842662f355950a10db84d853f74abf1f82942bd5
ana leite <anacarolinaleite1401@gmail.com>
2024-11-14 19:11:03 -0300

    parte do desgin feita, fazendo os ajustes

--- src/pages/pageAutenticator.jsx
+++ src/pages/pageAutenticator.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, {useContext, useEffect, useState} from "react";
 import Register from "../context/Register";
 import Login from "../context/Login";
 const PageAutenticator= ()=>{
+
+    const [click, setBtnId] = useState(null);
+    useEffect(()=>{
+        const container = document.getElementById("container");
+        const registerBtn = document.getElementById("register");
+        const loginBtn = document.getElementById("login");
+        registerBtn.addEventListener('click', ()=>{
+            container.classList.add("active");
+            
+        })
+
+        loginBtn.addEventListener('click', ()=>{
+            container.classList.remove("active");
+            
+        })
+        
+        
+    },[click]);
+    const handleClick = (e)=>{
+        setBtnId(e.target.id);
+    };
     return(
 
         <div className="container" id="container">
@@ -16,12 +37,12 @@ const PageAutenticator= ()=>{
                     <div className="toggle-panel toggle-left">
                         <h1>Bem vindo de volta!</h1>
                         <p>Insirar seus dados pessoas para acessar o nosso site</p>
-                        <button className="hidden" id="login">Sing In</button>
+                        <button className="hidden" id="login" onClick={handleClick}>Sing In</button>
                     </div>
                     <div className="toggle-panel toggle-right">
                         <h1>Olá, amigo!</h1>
                         <p>Registre seus dados pessoas para acessar o nosso site</p>
-                        <button className="hidden" id="register">Sing Up</button>
+                        <button className="hidden" id="register" onClick={handleClick}>Sing Up</button>
                     </div>
                 </div>
             </div>
